6a7084a5449aa13f75aeb94ccc60fb95209cce66
kangchaehyun <kht9831@naver.com>
2023-07-07 16:47:48 +0900

    move 2 files and create 2 files

--- jQuery/aaa.js
+++ jQuery/aaa.js
@@ -1,14 +1,13 @@
-// $('선택자)
+// $('선택자')
 // document.querySelector('선택자')
 
 console.log($("button"));
 $("button").css("color", "red");
 
-//  글자와 속성 변경
-
+// 글자와 속성 변경
 function submitjs() {
   // 안녕하세요 -> 반갑습니다.
-  // style 속성 border : 2px solid red;
+  // style 속성 border:2px solid red; (setAttribute)
   document.querySelector(".div1").innerHTML = "<b> 반갑습니다. </b>";
   document
     .querySelector(".div1")
@@ -16,6 +15,8 @@ function submitjs() {
 }
 
 function submitjq() {
-  $("div1").html("<i>잘 지내세요 여러분..</i>");
-  $("div1").css("border", "3px dotted blue");
+  // 안녕하세요 -> 잘지내세요 여러분
+  // border: 3px dotted blue;
+  $(".div1").html("<i>잘 지내세요 여러분..😢</i>");
+  $(".div1").css("border", "3px dotted blue");
 }
