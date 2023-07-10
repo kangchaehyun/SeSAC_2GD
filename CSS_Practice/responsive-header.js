376963f378ae425ce9945503825d0451d02fddcd
kangchaehyun <kht9831@naver.com>
2023-07-10 16:48:39 +0900

    jQuery & BOOTSTRAP

---
+++ CSS_Practice/responsive-header.js
@@ -0,0 +1,8 @@
+// window.addEventListener(`resize`, function () {
+//   const width = window.innerWidth;
+//   const height = window.innerHeight;
+//   if (width < 768) {
+//     const i = document.createElement("i");
+//     i.classList.add("fa-solid", "far-bars");
+//   }
+// });
